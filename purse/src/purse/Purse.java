af4967ebd9bee8950b67caaecf19e2ddadda64d9
LongyunYao <yaoyl753951@gmail.com>
2017-04-20 03:15:34 +0800

    RFID_lab1
    完善了实验一的代码，情况描述的更加清楚。所有测试全部9000

--- purse/src/purse/Purse.java
+++ purse/src/purse/Purse.java
@@ -8,10 +8,9 @@ import javacard.framework.JCSystem;
 import javacard.framework.Util;
 
 /**
- * 参考文献：http://blog.csdn.net/supergame111/article/details/5701106
- * 很全面
+ * 参考文献1：http://blog.csdn.net/supergame111/article/details/5701106（代码很全面）
+ * 参看文献2：http://blog.csdn.net/lv_victor/article/details/51028930  （讲解很详细）
  * @author yunyao
- *
  */
 
 public class Purse extends Applet {
@@ -20,8 +19,8 @@ public class Purse extends Applet {
 	
 	//文件系统
 	private KeyFile keyfile;            //密钥文件
-	private BinaryFile cardfile;       //应用基本文件
-	private BinaryFile personfile;     //持卡人基本文件
+	private BinaryFile cardfile;       	//应用基本文件
+	private BinaryFile personfile;     	//持卡人基本文件
 	private EPFile EPfile;              //电子钱包文件
 	
 	public Purse(byte[] bArray, short bOffset, byte bLength){
@@ -45,7 +44,7 @@ public class Purse extends Applet {
 			return;
 		}		
 		//步骤1:取APDU缓冲区数组引用并将之赋给新建数组
-		byte apdu_buffer[] = apdu.getBuffer();  // return null???
+		byte apdu_buffer[] = apdu.getBuffer();
 		//步骤2：取APDU缓冲区中数据放到变量papdu
 		//将apdu读取到卡片缓冲区当中并返回data段的长度  
 		short lc = apdu.setIncomingAndReceive();
@@ -54,18 +53,22 @@ public class Purse extends Applet {
         papdu.p1 = apdu_buffer[2];  
         papdu.p2 = apdu_buffer[3];  
         Util.arrayCopyNonAtomic(apdu_buffer, (short)5, papdu.pdata, (short)0, lc); 
-		//步骤3：判断命令APDU是否包含数据段，有数据则获取数据长度，并对le赋值
-        //否则，即不需要lc和data，则获取缓冲区原本lc实际上是le
-		//获取le的方法，因为不确定papdu有le部分，所以IOS7816下标可选项并没有le而是放在数据块中的.  
-		//如果有数据块，那le就是buffer[ISO7816.OFFSET_CDATA+lc]  
-		//调用papdu函数判断,不能直接通过lc判断,因为没lc只有le也会把le赋给lc  
-		if(papdu.APDUContainData()) {//若papdu命令包含数据块  
+		/*
+		 * 步骤3：判断命令APDU是否包含数据段，有数据则获取数据长度，并对le赋值
+		 * 否则，即不需要lc和data，则获取缓冲区原本lc实际上是le
+		 * 获取le的方法，因为不确定papdu有le部分，所以IOS7816下标可选项并没有le而是放在数据块中的. 
+		 * 如果有数据块，那le就是buffer[ISO7816.OFFSET_CDATA+lc]  
+		 * 调用papdu函数判断,不能直接通过lc判断,因为没lc只有le也会把le赋给lc  
+		 * 
+		 * 若papdu命令包含数据块  ，则需要更新le
+		 * 否则，将data的长度直接赋值给le
+		 * 注：LE字节表示的是期望卡片发回来的字节长度
+		 */
+		if(papdu.APDUContainData()) {
 		    papdu.le = apdu_buffer[ISO7816.OFFSET_CDATA+lc];  
-		    papdu.lc = apdu_buffer[ISO7816.OFFSET_LC];  
-		}  
-		else  
-		{  
-		    papdu.le = apdu_buffer[ISO7816.OFFSET_LC];//若没data部分则lc部分实际是le  
+		    papdu.lc = apdu_buffer[ISO7816.OFFSET_LC];
+		}  else {  
+		    papdu.le = apdu_buffer[ISO7816.OFFSET_LC];
 		    papdu.lc = 0;  
 		}  
 		// rc获取返回数据，判断操作是否成功
@@ -83,14 +86,15 @@ public class Purse extends Applet {
 	 * 功能：对命令的分析和处理
 	 * 参数：无
 	 * 返回：是否成功处理了命令
+	 * 《01 Java智能卡之概述》 P30
 	 */
 	private boolean handleEvent(){
 		switch(papdu.ins){
-			case condef.INS_CREATE_FILE:   	    return create_file(); 	// E0 文件创建
+			case condef.INS_CREATE_FILE:return create_file(); 				// E0 文件创建
 			//todo：完成写二进制命令，读二进制命令，写密钥命令
-            case (byte) 0xD4:  			        return write_key();  	// D4 写秘钥
-            case (byte) 0xD6:          			return write_binary();  // D6 写二进制文件
-            case (byte) 0xB0:          			return read_binary();  	// B0读二进制文件
+            case (byte) 0xD4:  			return write_key();  				// D4 写秘钥
+            case (byte) 0xD6:          	return write_read_bin((byte)'U');  	// D6 写二进制文件
+            case (byte) 0xB0:          	return write_read_bin((byte)'R');  	// B0读二进制文件
             /*
             case condef.INS_NIIT_TRANS:  
                 if(papdu.p1 == (byte)0x00)      return init_load();  
@@ -104,6 +108,127 @@ public class Purse extends Applet {
 		ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED); // 0x6D00 表示 CLA错误 
 		return false;
 	}
+	
+	/*
+	 * 写密钥  
+	 * 密钥文件是按照记录逐一写入的，也就是有几条密钥记录，就需要发送几条“写密钥”的命令。
+	 * 终端发送“写密钥”命令，卡片执行对应的操作，取出命令中的数据，并写入已建立的密钥文件中。
+	 */
+    private boolean write_key() {
+    	
+    	//每个应用只能有一个KEY文件，且必须最先建立
+        //《02 电子钱包的文件系统》P19
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
+        
+        if( papdu.cla != (byte)0x80 || papdu.ins != (byte)0xD4)
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
+  
+        //秘钥文件的p1为0x00， p2
+        if(papdu.p1 != (byte)0x00 || (papdu.p2 >= (byte)0x16 && papdu.p2 <= (byte)0x18))  
+            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);
+        
+        //秘钥长度是否正确
+        if(papdu.lc != (byte)0x15)
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
+  
+      //文件空间已满  
+        if(keyfile.recNum >= 3)
+            ISOException.throwIt(ISO7816.SW_FILE_FULL);  
+  
+        keyfile.addkey(papdu.p2, papdu.lc, papdu.pdata);//写入一条密钥  
+  
+        return true;  
+    }  
+
+    /*
+     * 读写选择器
+     * 《01 Java智能卡之概述》 P30
+     */
+    private boolean write_read_bin(byte U_R){
+    	switch(U_R){
+    		case (byte)'U': return write_binary();
+    		case (byte)'R': return read_binary();
+    		default : return false;
+    	}
+    }
+    
+    /**
+     * 写二进制文件
+     * 写二进制文件只需要一条命令
+     * 根据命令的内容获得需要参数
+     * 并写入已创建的持卡人基本文件或应用基本文件中；
+     * ！！！注意：写入前要先检测写入的数据是否超过文件限定的大小
+     * @return
+     */
+    private boolean write_binary() {  
+    	
+    	//首先判断cla是否确实为0x00，才能继续执行
+        if(papdu.cla != (byte)0x00)  
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
+    	
+        //每个应用只能有一个KEY文件，且必须最先建立
+        //《02 电子钱包的文件系统》P19
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);
+  
+        //写入前要先检测写入的数据是否超过文件限定的大小
+        //写入data长度应该在1-255之间
+        if(papdu.lc <= 0x00 && papdu.lc >= 0xFF)
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);
+
+        /*
+         * 持卡人或者卡不应该为空，如果不为空就可以开始写文件了
+         */
+        //应用基本文件为0x16，应用文件不应该为空
+        if(papdu.p1 == (byte)0x16 && cardfile == null)  
+            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+        //持卡人基本文件为0x17，持卡人不应该为null
+        if(papdu.p1 == (byte)0x17 && personfile == null)  
+            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
+
+        //应用基本文件为0x16， 持卡人基本文件为0x17
+        if(papdu.p1 == (byte)0x16){
+            cardfile.write_bineary(papdu.p2, papdu.lc, papdu.pdata); 
+        } else if(papdu.p1 == (byte)0x17){
+            personfile.write_bineary(papdu.p2, papdu.lc, papdu.pdata);  
+        }
+  
+        return true;  
+    }  
+    
+    /**
+     * 读取二进制文件
+     * 类似  
+     * @return
+     */
+    private boolean read_binary() {  
+    	
+    	//首先判断cla是否确实为0x00，才能继续执行
+        if(papdu.cla != (byte)0x00)  
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
+    	
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);
+
+        //应用基本文件为0x16，应用文件不应该为空
+        if(papdu.p1 == (byte)0x16 && cardfile == null)  
+            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+        //持卡人基本文件为0x17，持卡人不应该为null
+        if(papdu.p1 == (byte)0x17 && personfile == null)  
+            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
+  
+        //读取相应的二进文件  
+        if(papdu.p1 == (byte)0x16){
+            this.cardfile.read_binary(papdu.p2, papdu.le, papdu.pdata);
+        }
+        else if(papdu.p1 == (byte)0x17){
+            this.personfile.read_binary(papdu.p2, papdu.le, papdu.pdata);
+        }
+  
+        return true;  
+    }  
+	
 	/*
 	 * 功能：创建文件
 	 */
@@ -137,11 +262,40 @@ public class Purse extends Applet {
 		// 如果已经被分配
 		if(EPfile != null)
 			ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+        //每个应用只能有一个KEY文件，且必须最先建立
+        //《02 电子钱包的文件系统》P19
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
 		
 		EPfile = new EPFile(keyfile);
 		
 		return true;
 	}	
+
+	/*
+	 * 
+	 * 持卡人信息文件  
+	 */
+    private boolean Person_file() {  
+        if(papdu.cla != (byte)0x80)  
+            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
+  
+        if(papdu.lc != (byte)0x07)  
+            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
+  
+        if(personfile != null)
+            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);
+        
+        //每个应用只能有一个KEY文件，且必须最先建立
+        //《02 电子钱包的文件系统》P19
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
+  
+        //将数据写入personfile
+        personfile = new BinaryFile(papdu.pdata);
+  
+        return true;  
+    }  
 	    
     /*
      * Key_file()
@@ -158,128 +312,34 @@ public class Purse extends Applet {
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
   
         if(keyfile != null)
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
   
+        //keyfile在Key_file中创建，故不需要判断是否为null
         keyfile = new KeyFile();
   
         return true;  
     }  
     
-    //创建应用基本文件  
-    private boolean APP_file()  
-    {  
+    /*
+     * 应用 
+     * 创建应用基本文件  
+     */
+    private boolean APP_file() {  
+    	
         if(papdu.cla != (byte)0x80)  
             ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
   
         if(papdu.lc != (byte)0x07)  
             ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
   
-        if(cardfile != null)//有文件了还重复创建则会报错  
+        if(cardfile != null)
             ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-        if(keyfile == null)//都还没密钥文件（必须先于任何其他文件创建）  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
+        
+        if(keyfile == null)
+            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
   
-        this.cardfile = new BinaryFile(papdu.pdata);//传进的参数就是要写入的内容  
-  
-        return true;  
-    }  
-    
-    //创建持卡人信息文件  
-    private boolean Person_file()  
-    {  
-        if(papdu.cla != (byte)0x80)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        if(papdu.lc != (byte)0x07)  
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
-  
-        if(personfile != null)//有文件了还重复创建则会报错  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-        if(keyfile == null)//都还没密钥文件（必须先于任何其他文件创建）  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        this.personfile = new BinaryFile(papdu.pdata);//传进的参数就是要写入的内容  
-  
-        return true;  
-    }  
-	
-	//写入一条密钥  
-    private boolean write_key()  
-    {  
-        if(keyfile == null)//都还没密钥文件  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        if(papdu.cla != (byte)0x80)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        //文件标识有错,这句判断写得有问题啊,老是会返回这个异常--已解决，应该用and而不是or，因为要三个情况都不是才异常  
-        if(papdu.p2 != (byte)0x06 && papdu.p2 != (byte)0x07 && papdu.p2 != (byte)0x08)  
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);  
-  
-        if(papdu.lc == 0 || papdu.lc > 21)//密钥长度不能为0也不能超过21  
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
-  
-        if(keyfile.recNum >= 3)//文件空间已满  
-            ISOException.throwIt(ISO7816.SW_FILE_FULL);  
-  
-        this.keyfile.addkey(papdu.p2, papdu.lc, papdu.pdata);//写入一条密钥  
-  
-        return true;  
-    }  
-
-    //写入二进制文件  
-    private boolean write_binary()  
-    {  
-        if(keyfile == null)//都还没密钥文件  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        //都还没二进制文件--没找到  
-        if(cardfile == null && papdu.p1 == (byte)0x16)  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-        if(personfile == null && papdu.p1 == (byte)0x17)  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        if(papdu.cla != (byte)0x00)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        /*if(papdu.p2 == 0)//没有文件标识 
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);*/  
-  
-        if(papdu.lc == 0)//写入长度不能为0  
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
-  
-        //写入一条二进制命令到文件  
-        if(papdu.p1 == (byte)0x16)//表明写入的是应用信息  
-            this.cardfile.write_bineary(papdu.p2, papdu.lc, papdu.pdata);  
-        else if(papdu.p1 == (byte)0x17)//表明写入的是持卡人信息  
-            this.personfile.write_bineary(papdu.p2, papdu.lc, papdu.pdata);  
-  
-        return true;  
-    }  
-    
-    //读取二进制文件  
-    private boolean read_binary()  
-    {  
-        if(keyfile == null)//都还没密钥文件  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        //都还没二进制文件--没找到  
-        if(cardfile == null && papdu.p1 == (byte)0x16)  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-        if(personfile == null && papdu.p1 == (byte)0x17)  
-            ISOException.throwIt(ISO7816.SW_CONDITIONS_NOT_SATISFIED);  
-  
-        if(papdu.cla != (byte)0x00)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        /*if(papdu.p2 == 0)//没有说明读取文件偏移量 
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);*/  
-  
-        //读取相应的二进文件  
-        if(papdu.p1 == (byte)0x16)//表明读取的是应用文件  
-            this.cardfile.read_binary(papdu.p2, papdu.le, papdu.pdata);  
-        else if(papdu.p1 == (byte)0x17)//表明读取的是持卡人信息文件  
-            this.personfile.read_binary(papdu.p2, papdu.le, papdu.pdata);  
+        //将数据写入cardfile
+        cardfile = new BinaryFile(papdu.pdata);
   
         return true;  
     }  
@@ -312,7 +372,6 @@ public class Purse extends Applet {
 			ISOException.throwIt(ISO7816.SW_RECORD_NOT_FOUND);
 		
 		papdu.le = (short)4;
-		//papdu.le = (short)16; //正确为16
 		
 		return true;
 	}
@@ -354,53 +413,12 @@ public class Purse extends Applet {
 	 * 功能：消费命令的实现
 	 */
 	private boolean purchase(){
-        short rc;  
-        
-        if(papdu.cla != (byte)0x80)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        if(papdu.p1 != (byte)0x01 && papdu.p2 != (byte)0x00)  
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);  
-  
-        if(EPfile == null)  
-            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
-  
-        if(papdu.lc != (short)0x0F)  
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
-            //测试所用//ISOException.throwIt(papdu.lc);  
-  
-        rc = EPfile.purchase(papdu.pdata);  
-  
-        if(rc == 1)//MAC1验证未通过  
-            ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);  
-        else if(rc == 2)  
-            //ISOException.throwIt(condef.SW_BALANCE_NOT_ENOUGH);
-        	ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);  
-        else if(rc == 3)  
-            ISOException.throwIt(ISO7816.SW_RECORD_NOT_FOUND);  
-  
-        papdu.le = (short)8;//正确是8  
-        //papdu.le = (short)38;//测试  
 		return true;
 	}
 	/*
 	 * 功能：余额查询功能的实现
 	 */
 	private boolean get_balance(){
-		if(papdu.cla != (byte)0x80)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        if(papdu.p1 != (byte)0x01 && papdu.p2 != (byte)0x02)  
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);  
-  
-        short result;  
-        byte[] balance = JCSystem.makeTransientByteArray((short)4, JCSystem.CLEAR_ON_DESELECT);//余额暂存  
-        result = EPfile.get_balance(balance);  
-  
-        if(result == (short)0)  
-            Util.arrayCopyNonAtomic(balance, (short)0, papdu.pdata, (short)0, (short)4);//余额data[0]~data[3]  
-  
-        papdu.le = (short)0x04;  
 		return true;
 	}
 	
@@ -408,33 +426,6 @@ public class Purse extends Applet {
 	 * 功能：消费初始化的实现
 	 */
 	private boolean init_purchase(){
-		short num,rc;  
-		  
-        if(papdu.cla != (byte)0x80)  
-            ISOException.throwIt(ISO7816.SW_CLA_NOT_SUPPORTED);  
-  
-        if(papdu.p1 != (byte)0x01 && papdu.p2 != (byte)0x02)  
-            ISOException.throwIt(ISO7816.SW_WRONG_P1P2);  
-  
-        if(papdu.lc != (short)0x0B)  
-            ISOException.throwIt(ISO7816.SW_WRONG_LENGTH);  
-  
-        if(EPfile == null)  
-            ISOException.throwIt(ISO7816.SW_FILE_NOT_FOUND);  
-  
-        num = keyfile.findkey(papdu.pdata[0]);//根据tag寻找密钥返回密钥的记录号  
-  
-        if(num == 0x00)//表示找不到相应密钥  
-            ISOException.throwIt(ISO7816.SW_RECORD_NOT_FOUND);  
-  
-        rc = EPfile.init4purchase(num, papdu.pdata);//返回0表示成功,返回2表示余额不足  
-  
-        if(rc == 2)  
-            //ISOException.throwIt(condef.SW_BALANCE_NOT_ENOUGH);
-        	ISOException.throwIt(ISO7816.SW_SECURITY_STATUS_NOT_SATISFIED);  
-  
-        papdu.le = (short)15;  
-  
 		return true;
 	}
 }
